d715fd6627257fec20d413d96c5c09edf725bf36
Chikaturin <ninhhoaithanh@gmail.com>
2024-09-27 14:13:00 +0700

    "Update welcome message in root route to 'Welcome to Rental 4 U'"

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use((err, req, res, next) => {
 });
 
 app.get("/", (req, res) => {
-  res.json({ status: "Welcome to Voucher Management System" });
+  res.json({ status: "Welcome to Rental 4 U" });
 });
 
 app.use("/api", require("./Router/AccountRouter.js"));
