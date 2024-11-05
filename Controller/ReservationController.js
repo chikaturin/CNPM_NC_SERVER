d0f061c36e33151f5074862fb50af1f38f1f09aa
Chikaturin <ninhhoaithanh@gmail.com>
2024-11-06 01:12:55 +0700

    "Removed unnecessary object wrapper in JSON response"

--- Controller/ReservationController.js
+++ Controller/ReservationController.js
@@ -20,7 +20,7 @@ const dateContract = async (req, res) => {
       let desiredDate = new Date(Reservation.Desired_Date);
       desiredDate.setDate(desiredDate.getDate() - 1);
       date = formatDate(desiredDate);
-      return res.status(201).json({ date });
+      return res.status(201).json(date);
     }
     res.status(200).json(date);
   } catch (error) {
